4aec04cccc0d85bd36f0d7cd684db18d6e2339e3
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 20:03:00 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan13-Relational/src/pboulg/pkg10115557/latihan13/relational/PBOULG10115557Latihan13Relational.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan13.relational;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 13
+ */
+public class PBOULG10115557Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+    	int bil2 = 20;
+	System.out.println("a == b = " + (bil1 == bil2));
+	System.out.println("a != b = " + (bil1 != bil2));
+	System.out.println("a > b = " + (bil1 > bil2));
+	System.out.println("a < b = " + (bil1 < bil2));
+	System.out.println("b >= a = " + (bil2 >= bil1));
+	System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+}
